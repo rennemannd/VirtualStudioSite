e7bc15f9de3de80f186e2d9a0cc6bb95e286605f
Jonathan Conlin <jconlin@ufl.edu>
2019-07-28 13:44:40 -0400

    profile card design in progress

--- src/views/Team/Team.js
+++ src/views/Team/Team.js
@@ -1,8 +1,7 @@
 import React from "react";
-import "./css/rotating-card.css";
 import Deveon from "./Images/Headshots/DeVeon.jpg";
 import Caylem from "./Images/Headshots/Caylem.png";
-import "./css/pe-icon-7-stroke.css";
+import "./Team.scss";
 
 class Team extends React.Component {
 
@@ -10,75 +9,66 @@ class Team extends React.Component {
         return (
             <html>
             <div className="card-container">
-                <div className="card">
-                    <div className="front">
-                        <div className="cover">
-                            <img src={Caylem}/>
-                        </div>
-                        <div className="user">
-                            <img className="img-circle" src={Deveon}/>
-                        </div>
-                        <div className="content">
-                            <div className="main">
-                                <h3 className="name">DeVeon</h3>
-                                <p className="profession">VFX and Animation</p>
+                <span className="pro">VFX</span>
+                <div className={"image"}>
+                    <img
+                        className="round"
+                        src={Deveon}
+                        alt="user"/>
+                </div>
+                <h3>Deveon</h3>
+                <h6>A place.</h6>
+                <p>
+                    VFX and 2D/3D animations <br/>
+                    creator.
+                </p>
+                <div className="buttons">
 
-                                <p className="text-center">"I'm the new Sinatra, and since I made it here I can make it
-                                    anywhere, yeah, they love me everywhere"</p>
-                            </div>
-                            <div className="footer">
-                                <div className="rating">
-                                    <i className="fa fa-mail-forward"/> Auto Rotation
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="back">
-                        <div className="header">
-                            <h5 className="motto">"To be or not to be, this is my awesome motto!"</h5>
-                        </div>
-                        <div className="content">
-                            <div className="main">
-                                <h4 className="text-center">Job Description</h4>
-                                <p className="text-center">Web design, Adobe Photoshop, HTML5, CSS3, Corel and many
-                                    others...</p>
-
-                                <div className="stats-container">
-                                    <div className="stats">
-                                        <h4>235</h4>
-                                        <p>
-                                            Followers
-                                        </p>
-                                    </div>
-                                    <div className="stats">
-                                        <h4>114</h4>
-                                        <p>
-                                            Following
-                                        </p>
-                                    </div>
-                                    <div className="stats">
-                                        <h4>35</h4>
-                                        <p>
-                                            Projects
-                                        </p>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-                        <div className="footer">
-                            <div className="social-links text-center">
-                                <a href="https://creative-tim.com" className="facebook"><i
-    className="fa fa-facebook fa-fw"/></a>
-                                <a href="https://creative-tim.com" className="google"><i
-    className="fa fa-google-plus fa-fw"/></a>
-                                <a href="https://creative-tim.com" className="twitter"><i
-    className="fa fa-twitter fa-fw"/></a>
-                            </div>
-                        </div>
-                    </div>
+                </div>
+                <div className="skills">
+                    <h6>Skills</h6>
+                    <ul>
+                        <li>UI / UX</li>
+                        <li>Front End Development</li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                        <li>React</li>
+                        <li>Node</li>
+                    </ul>
                 </div>
             </div>
+            <div className="card-container">
+                <span className="pro">VFX</span>
+                <div className={"image"}>
+                    <img
+                        className="round"
+                        src={Deveon}
+                        alt="user"/>
+                </div>
+                <h3>Deveon</h3>
+                <h6>A place.</h6>
+                <p>
+                    VFX and 2D/3D animations <br/>
+                    creator.
+                </p>
+                <div className="buttons">
+
+                </div>
+                <div className="skills">
+                    <h6>Skills</h6>
+                    <ul>
+                        <li>UI / UX</li>
+                        <li>Front End Development</li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                        <li>React</li>
+                        <li>Node</li>
+                    </ul>
+                </div>
+            </div>
+
             </html>
         )
     }
